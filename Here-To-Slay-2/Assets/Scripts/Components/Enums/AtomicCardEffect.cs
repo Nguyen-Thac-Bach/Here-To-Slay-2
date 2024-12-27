23696f165392b9e17f0ca805507d45e96e2423ac
Nguyen-Thac-Bach <129993123+Nguyen-Thac-Bach@users.noreply.github.com>
2024-12-27 21:04:16 +0100

    add atomiccardeffect

---
+++ Here-To-Slay-2/Assets/Scripts/Components/Enums/AtomicCardEffect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Enums
+{
+    /// <summary>
+    /// These are the smallest possible effects that a card can have.
+    /// </summary>
+    /// <remarks>Categorized in comments based on 3 criteria: 
+    /// needsChoosingCard (by user), needsChoosingDestination, actorRelevant</remarks>
+    /// TODO: Implement AtomicCardCondition enum
+    public enum AtomicCardEffect
+    {
+
+        //neither
+        ReplenishAttackableMonsters,
+        //actorRelevant
+        Draw,
+        //needsChoosingCard
+        Recall,
+        //needsChoosingCard, actorRelevant
+        Discard,
+        PlaySpell,
+        PlayHero,
+        PlayModifier,
+        PlayChallenge,
+        Destroy,
+        StealHero,
+        Sacrifice,
+        Slay,
+        Give,
+        StealFromHand,
+        //needsChoosingCard, needsChoosingDestination, actorRelevant
+        PlayItem
+        
+        //Restore - if ever implemented, it would dig for a card in the discard pile and put it back in the hand
+        
+        
+    }
+}
